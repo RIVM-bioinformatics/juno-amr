593a95f7c53346653dbc2ae9027c47ba65c731d2
Roxanne Wolthuis <wolthuir@rivm-biohn-l05p.int.ssc-campus.nl>
2021-10-12 10:55:40 +0200

    Added additional piece of code for the metadata

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -87,6 +87,24 @@ python juno-amr.py --queue "${QUEUE}" -i "${input_dir}" -o "${output_dir}" -s "$
 
 result=$?
 
+# Propagate metadata
+# SEQ_KEYS=
+# SEQ_ENV=`env | grep irods_input_sequencing`
+# for SEQ_AVU in ${SEQ_ENV}
+# do
+#     SEQ_KEYS="${SEQ_KEYS} ${SEQ_AVU%%=*}"
+# done
+
+# for key in $SEQ_KEYS irods_input_illumina__Flowcell irods_input_illumina__Instrument \
+#     irods_input_illumina__Date irods_input_illumina__Run_number irods_input_illumina__Run_Id
+# do
+#     if [ ! -z ${!key} ] ; then
+#         attrname=${key:12}
+#         attrname=${attrname/__/::}
+#         echo "${attrname}: '${!key}'" >> ${OUTPUTDIR}/metadata.yml
+#     fi
+# done
+
 exit ${result}
 # Produce svg with rules
 # snakemake --config sample_sheet=config/sample_sheet.yaml \
